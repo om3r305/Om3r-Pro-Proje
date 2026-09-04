2d78fc8b73e9be22387d63afc25c5d03dbaa8335
om3r305 <omer.30588@gmail.com>
2026-09-04 11:25:56 +0200

    feat(dashboard): add PWA service worker

---
+++ monster-coins-pro/sw.js
@@ -0,0 +1,12 @@
+const CACHE='monster-coins-pro-shell-v1';
+const ASSETS=['/','/manifest.webmanifest','/logo.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match(event.request).then(response=>response||caches.match('/'))));
+});
